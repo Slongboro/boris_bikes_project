ac94c3322dff5ebf08526f0f3e4c6825a3c2c49b
slongboro <susanlongley@gmail.com>
2019-10-16 15:35:55 +0100

    Error code for no bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,21 +1,32 @@
 require 'docking_station'
 describe DockingStation do
-    it "should respond to the method release_bike" do
-expect(subject).to respond_to(:release_bike)
-    end
-    it 'releases working bikes' do
-        bike = subject.release_bike
-expect(bike).to be_working
-    end
+  it 'should respond to the method release_bike' do
+    expect(subject).to respond_to(:release_bike)
+  end
+  it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
+    expect(bike).to be_working
+  end
 
-    it "should respond to dock bike with one argument" do
+  it 'should release a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
+
+  it 'should respond to dock bike with one argument' do
     expect(subject).to respond_to(:dock).with(1).argument
-    end
+  end
 
-    it "should dock a bike" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
-    end
+  it 'should dock a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
- end
+  it 'should give an error if no bike' do
+  expect { subject.release_bike }.to raise_error 'No bikes available'
+end
+end
