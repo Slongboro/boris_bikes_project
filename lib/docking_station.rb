df614baf50867a780ba4602aadda6626bc02c900
slongboro <susanlongley@gmail.com>
2019-10-16 14:44:54 +0100

    Dock bike test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,7 +5,9 @@ class DockingStation
     def release_bike
         Bike.new
     end
-    
+
     def dock(bike)
+      bike
     end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,22 +1,21 @@
 require 'docking_station'
-describe DockingStation do 
+describe DockingStation do
     it "should respond to the method release_bike" do
-        docking_station = DockingStation.new
-expect(docking_station).to respond_to(:release_bike)
+expect(subject).to respond_to(:release_bike)
     end
     it 'releases working bikes' do
-        docking_station = DockingStation.new
-        bike = docking_station.release_bike
+        bike = subject.release_bike
 expect(bike).to be_working
     end
-#     it "should respond to dock bike" do
-#         docking_station = DockingStation.new
-#         dock = docking_station.dock
-# expect(docking_station).to respond_to(:dock)
-#     end
 
     it "should respond to dock bike with one argument" do
-        dock = DockingStation.new
-    expect(dock).to respond_to(:dock).with(1).argument
+    expect(subject).to respond_to(:dock).with(1).argument
     end
+
+    it "should dock a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+
  end
